12cf8b20b7af2f7c5ff53f23a9104141e3b8b384
Adaya <165713056+Adayarith@users.noreply.github.com>
2025-03-25 21:39:35 -0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
-
+import { useNavigate } from 'react-router-dom';
 
 function Navbar() {
+  const navigate = useNavigate();
+
   return (
-    <nav className="bg-white p-2 shadow-md fixed bottom-0 left-0 w-full flex justify-around">
-      <button aria-label="Menu" className="p-2 hover:bg-gray-100 rounded-md">
-      <img src="/src/assets/application.png" alt="Logo de la aplicación" className="h-8" />
+    <nav className="bg-white p-2 shadow-md fixed bottom-0 left-0 w-full flex justify-around z-20">
+      <button
+        aria-label="Inicio"
+        className="p-2 hover:bg-gray-100 rounded-md"
+        onClick={() => navigate('/')}
+      >
+        <img src="/src/assets/Menu.jpg" alt="Menú" className="h-8" />
       </button>
-      <button aria-label="Dashboard" className="p-2 hover:bg-gray-100 rounded-md">
-      <img src="/src/assets/file.png" alt="Logo de la aplicación" className="h-8" />
-      </button>
-      <button aria-label="Settings" className="p-2 hover:bg-gray-100 rounded-md">
-      <img src="/src/assets/configuraciones.png" alt="Logo de la aplicación" className="h-8" />
+      <button
+        aria-label="Historial"
+        className="p-2 hover:bg-gray-100 rounded-md"
+      >
+        <img src="/src/assets/Historial.jpg" alt="Historial" className="h-8" />
       </button>
     </nav>
   );
