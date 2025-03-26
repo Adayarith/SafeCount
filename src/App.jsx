34d16be05a79cefb1172bbb7f26ab0e379b87f96
Adaya <165713056+Adayarith@users.noreply.github.com>
2025-03-25 21:47:29 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
-import ConfigScreen from "./configscreen";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from './components/Dashboard';
+import Usuario from "./components/Perfil"; // 
+import ConfigScreen from './components/ConfigScreen';
 
 function App() {
-  return <ConfigScreen />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/perfil" element={<Usuario />} />
+        <Route path="/config" element={<ConfigScreen />} /> 
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
